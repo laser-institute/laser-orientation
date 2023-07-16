bbc50d68a866a2a2be990dad8f2d4ab6023facf4
sbkellogg <sbkellog@ncsu.edu>
2023-07-16 12:44:19 -0400

    laser orientation done

--- laser-lab-script.R
+++ laser-lab-script.R
@@ -19,3 +19,30 @@ library(tidyverse)
 sci_data <- read_csv("data/sci-online-classes.csv", 
                      col_names = TRUE)
 
+# print data to console to view
+sci_data
+
+
+
+# WRANGLE ####
+
+# select student_id and course_id and save as 
+sci_data |> 
+  select(FinalGradeCEMS, TimeSpent)
+
+
+
+# EXPLORE ####
+
+# make a scatter plot showing time spent and final grades
+ggplot(sci_data) +
+  geom_point(aes(x = TimeSpent, y = FinalGradeCEMS))
+
+
+
+# MODEL ####
+
+# use a simple linear regression model to predict final course grades
+m1 <- lm(FinalGradeCEMS ~ TimeSpent, data = sci_data)
+summary(m1)
+
